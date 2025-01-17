39dff84053400bfb5d1f86fd1c0bd2ce0323b650
Shaira <sil21019@byui.edu>
2025-01-17 15:31:09 +0000

    bootstrap installed

--- ui/watchlist-app/src/main.jsx
+++ ui/watchlist-app/src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
